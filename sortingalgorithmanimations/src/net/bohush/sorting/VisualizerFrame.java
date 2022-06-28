80dfe7c73e3a445af8107c555ed20974f721ef3f
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2022-06-28 19:51:03 +0700

    Update VisualizerFrame.java

--- sortingalgorithmanimations/src/net/bohush/sorting/VisualizerFrame.java
+++ sortingalgorithmanimations/src/net/bohush/sorting/VisualizerFrame.java
@@ -201,17 +201,3 @@ public class VisualizerFrame extends JFrame {
 		validate();
 	}
 
-}
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
